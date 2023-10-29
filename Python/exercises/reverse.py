c96c39f1e793aeb8cbf31a67c8c8cd62fb1b37d3
Armend Shala <armenddsh@gmail.com>
2023-10-29 16:14:21 +0100

    Update reverse.py

--- Python/exercises/reverse.py
+++ Python/exercises/reverse.py
@@ -1,27 +1,39 @@
 # Reversing an array in-place exercise
 
+
 # Reversing an array in-place exercise
+
 
 # In this exercise, you have to reverse a list in O(N) linear time complexity and we want the algorithm to be in-place as well - so the algorithm can not use additional memory (it means you have to manipulate the input list and not create an independent list)!
 
+
 # For example: input is [1,2,3,4,5] then the output is [5,4,3,2,1]
+
 
 # Good luck!
 
+
 def reverse(nums):
+
     startIndex = 0
+
     lastIndex = len(nums) - 1
 
+
     while startIndex < lastIndex:
+
         nums[startIndex], nums[lastIndex] = nums[lastIndex], nums[startIndex]
 
+
         startIndex = startIndex + 1
+
         lastIndex = lastIndex - 1
     
     return nums
 
-def reverse_integer(number):
-    number_str = str(number)
+
+def reverse_integer(n):
+    number_str = str(n)
     number_array = list(number_str)
     startIndex = 0
     lastIndex = len(number_array) - 1
